ffd37ab219fe34ce5683fcb7ec77d63444f08595
Felan Carlo Garcia <felancarlogarcia@gmail.com>
2016-09-26 23:10:50 +0800

    Update model_training.py

--- model_training/model_training.py
+++ model_training/model_training.py
@@ -6,7 +6,7 @@ import numpy  as np
 import pandas as pd
 from  sklearn.neighbors import KNeighborsClassifier
 
-path  = 'appdata.csv'
+path  = 'data/appdata.csv'
 train = pd.read_csv(path, sep=',')
 
 # remove the time and category
